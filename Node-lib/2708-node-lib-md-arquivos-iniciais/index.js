a306ec37439398d9d7f063d97b2f2c8b46e5d7fa
fernandagcarmo <161831771+fernandagcarmo@users.noreply.github.com>
2025-10-01 07:26:03 -0300

    Add files via upload

---
+++ Node-lib/2708-node-lib-md-arquivos-iniciais/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('ola mundo'))
+console.log(chalk.cyan('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input)'))
+console.log(chalk.magenta('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input)'))
